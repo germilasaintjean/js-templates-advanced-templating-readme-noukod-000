989b98cd2df5a810411071a0d4ac11fe8db6727b
Saint-Jean Germila <germila@noukod.com>
2019-03-27 15:23:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function loadIssue() {
 
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
    for(var i=0;i<issues.length;i++) {
-     var result = template(issues[i]);
-     document.getElementsByTagName("main")[0].innerHTML += result;
-   }
+     var result = template(issues);
+   document.getElementsByTagName("main")[0].innerHTML += result;
  }
